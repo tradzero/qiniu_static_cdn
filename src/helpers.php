0faeb679e0cd3d2b6a2b8b0f9bc817367407fb3a
林振宇 <admin@drakframe.com>
2017-04-18 16:51:05 +0800

    helper注释

--- src/helpers.php
+++ src/helpers.php
@@ -1,7 +1,14 @@
 <?php
 
 if (!function_exists('cdnAsset')) {
-    function cdnAsset($path, $prefix = "public")
+    /**
+     * 根据路径拼接CDN文件地址
+     *
+     * @param string $path 文件相对路径
+     * @param string $prefix 选择相对于laravel文件目录的位置 默认为public
+     * @return string path 
+     */
+    function cdnAsset($path, $prefix="public")
     {
         $domain = env('QINIU_URL', '');
         if (0 !== stripos($domain, 'https://') && 0 !== stripos($domain, 'http://')) {
