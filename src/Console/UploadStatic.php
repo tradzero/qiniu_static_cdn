64188a3f2ee6ca0d6abb036119b00f640650eac0
林振宇 <admin@drakframe.com>
2017-04-18 12:35:12 +0800

    上传逻辑交给command处理

--- src/Console/UploadStatic.php
+++ src/Console/UploadStatic.php
@@ -4,10 +4,12 @@ namespace Tradzero\Uploader\Console;
 
 use Tradzero\Uploader\Uploader;
 use Illuminate\Console\Command;
+use File;
 
 class UploadStatic extends Command
 {
-    protected $signature = 'uploader:upload {--folder=}';
+    protected $signature = 'uploader:upload
+        {--folder=public : 选择上传的文件夹}';
 
     protected $description = '初始化上传所有静态文件';
 
@@ -32,6 +34,25 @@ class UploadStatic extends Command
 
     public function handle()
     {
-        $this->uploader->uploadFolderFiles('public');
+        $folder = $this->option('folder');
+        if (! File::exists($folder)) {
+            $this->error('文件夹可能不存在');
+            return ;
+        }
+        $this->uploader->initFolderUpload($folder);
+        $bar = $this->output->createProgressBar($this->uploader->getFileCount());
+        $files = $this->uploader->getFiles();
+        foreach ($files as $item => $file) {
+            $hash = $this->uploader->checkFileLastestOrUpload($file);
+            if (!is_null($hash)) {
+                $bar->advance();
+            } else {
+                $bar->finish();
+                $this->error('上传出错');
+                return ;
+            }
+        }
+        $bar->finish();
+        $this->info('上传成功');
     }
 }
